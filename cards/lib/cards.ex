dc50b09c025f4e2cd4e37e94c013c9ffa97cc139
= <=>
2023-04-07 18:07:13 +0300

    list comprehesion

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,16 +1,17 @@
 defmodule Cards do
 
   def create_deck do
-    ["ace", "king", "queen", "jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+   values = ["ace", "king", "queen", "jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+   suits = ["hearts", "diamonds", "spades", "clubs"]
+   newList = for suit <- suits, value <- values, do: "#{value} of #{suit}" #for suit in suits, value in values, do: "#{value} of #{suit}"
   end
 
   def shuffle_deck(deck) do
-    Enum.shuffle(deck) # Enum is a module that has a function called shuffle
+    Enum.shuffle(deck) #shuffle returns a shuffled list
   end
 
-  def shuffle_deck do
-    deck = create_deck()
-    shuffle_deck(deck)
+  def contains?(deck, card) do # the ? is a convention to indicate that the function returns a boolean
+    Enum.member?(deck, card) #member? returns true if the list contains the given element
   end
 
   def save_deck(deck) do
