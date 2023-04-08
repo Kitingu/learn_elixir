2fc38f1d31d4573eda2f341d4adddd3d9a85f73f
= <=>
2023-04-08 05:02:22 +0300

    save deck to binary file

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -10,11 +10,19 @@ defmodule Cards do
     Enum.shuffle(deck) #shuffle returns a shuffled list
   end
 
+  def deal(deck,hand_size) do
+    #split returns a tuple two lists with the first list containing the first n elements of the list
+    Enum.split(deck, hand_size) #split returns a tuple with two lists
+    {hand, new_deck} = Enum.split(deck, hand_size) #pattern match the tuple with the two lists
+
+  end
+
   def contains?(deck, card) do # the ? is a convention to indicate that the function returns a boolean
     Enum.member?(deck, card) #member? returns true if the list contains the given element
   end
 
-  def save_deck(deck) do
-    File.write("deck.txt", deck)
+  def save_deck(deck,filename) do
+    binary = :erlang.term_to_binary(deck) #term_to_binary returns a binary representation of the given term
+    File.write(filename, binary) #write writes the given binary to the given file
   end
 end
