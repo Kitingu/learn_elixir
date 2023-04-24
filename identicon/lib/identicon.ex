61290dcdea138923dab95d06ac7ef8ba778fb965
= <benlegendj@gmail.com>
2023-04-24 20:43:05 +0300

    creating a pixel map

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,6 +5,7 @@ defmodule Identicon do
     |> pick_color #has the Identicon.Image struct as an argument
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
   end
   @doc """
   Takes an Identicon.Image struct and returns an updated struct with a color tuple
@@ -33,6 +34,17 @@ defmodule Identicon do
     end
     %Identicon.Image{image | grid: grid}
 
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image)do
+    pixel_map =Enum.map(grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+      {top_left, bottom_right}
+    end)
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def mirror_row(row) do
--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -1,4 +1,4 @@
 defmodule Identicon.Image do
-  defstruct hex: nil, color: nil, grid: nil 
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
   #struct is a map with a defined set of keys
 end
