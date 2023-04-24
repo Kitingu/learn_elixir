08e298bf6b8b0955e66e9844c5fd8aa17ab33cdd
= <benlegendj@gmail.com>
2023-04-24 08:51:08 +0300

    setup identicon project

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,14 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> to_hash
+  end
+  def to_hash(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+  end
+
+
+
+
+end
