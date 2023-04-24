ab7e26f42b9fbdf06fc97bc44aec33ee7882eca6
= <benlegendj@gmail.com>
2023-04-24 22:59:02 +0300

    draw image and save using egd

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,6 +6,8 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input) #input is the filename
   end
   @doc """
   Takes an Identicon.Image struct and returns an updated struct with a color tuple
@@ -45,6 +47,20 @@ defmodule Identicon do
       {top_left, bottom_right}
     end)
     %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250,250)
+    fill = :egd.color(color)
+    Enum.each(pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+    #Enum.each is like Enum.map but it returns :ok
+    :egd.render(image)
+  end
+
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
   end
 
   def mirror_row(row) do
