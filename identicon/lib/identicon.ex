db6d54442928097c1e86152082bbca64b44d1a6d
= <benlegendj@gmail.com>
2023-04-24 16:04:54 +0300

    mirroring a row

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,10 +3,11 @@ defmodule Identicon do
     input
     |> to_hash  #calls next function passing in the result of the previous function
     |> pick_color #has the Identicon.Image struct as an argument
+    |> build_grid
   end
   @doc """
   Takes an Identicon.Image struct and returns an updated struct with a color tuple
-  
+
   """
   def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
     #shorthand for pattern matching
@@ -14,6 +15,18 @@ defmodule Identicon do
     %Identicon.Image{image | color: {r,g,b}} #update the image struct with the color tuple
 
   end
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3,3, :discard) #chunk_by returns a list of lists with the given number of elements
+
+  end
+
+  def mirror_row(row) do
+    [first,second | _tail] = row
+    row ++ [second,first]
+
+  end
+
   def to_hash(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
