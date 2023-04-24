95983f11a7c9f3a515288f25561522e1f8989a2c
= <benlegendj@gmail.com>
2023-04-24 11:16:19 +0300

    pattern matching struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,13 @@ defmodule Identicon do
   def main(input) do
     input
     |> to_hash  #calls next function passing in the result of the previous function
+    |> pick_color #has the Identicon.Image struct as an argument
+  end
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list #pattern matching to get the first three elements of the list
+    # | _tail is a convention to indicate that we don't care about the rest of the list
+    [r, g, b]
   end
   def to_hash(input) do
     hex = :crypto.hash(:md5, input)
